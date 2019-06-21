4c2bf4e1d4c540d67312eef6442c697f66341d44
wchatx <wchatx@gmail.com>
2019-06-21 06:49:13 -0500

    stream wip, joblib

--- cheapodb/stream.py
+++ cheapodb/stream.py
@@ -4,13 +4,10 @@ from itertools import islice, chain
 from typing import Union, Generator, List
 
 import boto3
-from sqlalchemy import create_engine
-from sqlalchemy.engine import Connectable, ResultProxy
-from sqlalchemy.sql import Selectable
 from joblib import Parallel, delayed
 
 
-class StreamPipeline(object):
+class Stream(object):
     def __init__(self, name: str):
         self.name = name
         self.firehose = boto3.client('firehose')
@@ -47,37 +44,9 @@ class StreamPipeline(object):
         for first in iterator:
             yield chain([first], islice(iterator, size - 1))
 
-    def from_sql(self, sql: Union[str, Selectable], conn: Union[str, Connectable],
-                 execution_options: dict = None, chunk_size: int = 500, threads: int = 4):
-        """
-        Ingest from RDBMS using sqlalchemy
-
-        :param sql:
-        :param conn:
-        :param execution_options:
-        :param chunk_size:
-        :param threads:
-        :return:
-        """
-        if isinstance(conn, str):
-            conn = create_engine(conn, execution_options)
-
-        result: ResultProxy = conn.execute(sql)
-        while True:
-            records = result.fetchmany(chunk_size)
-            if not records:
-                break
-
-            Parallel(n_jobs=threads, prefer='threads')(delayed(self.firehose.put_record_batch)(
-                DeliveryStreamName=self.name,
-                Records=[{'Data': json.dumps(dict(x))} for x in chunk]
-            ) for chunk in self._chunks(records, size=500))
-
-        return
-
     def from_records(self, records: Union[Generator, List[dict]], threads: int = 4) -> None:
         """
-        Ingest from an iterable of dicts
+        Ingest from a generator or list of dicts
 
         :param records: a generator or list of dicts
         :param threads: number of threads for batch putting
